df52d9136f2271046f8fe0477f421d0157f6aa74
Luca Trautmann <guy_montag@posteo.net>
2023-08-26 21:16:56 +0200

    Add foundational work on von Mises loss

---
+++ src/vector_reconstruction.py
@@ -0,0 +1,37 @@
+import torch
+import numpy as np
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class VectorReconstruction(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.model = nn.Sequential(
+                nn.Linear(1,16),
+                nn.ReLU(),
+                nn.Linear(16,1),)
+
+    def forward(self, x):
+        x = self.model(x)
+        return torch.atan2(torch.sine(x), torch.cosine(x))
+
+# Hyperparameters
+epochs = 100
+
+def vonMisesLoss(output, target):
+    return torch.sum(1 - torch.exp(torch.cos(output - target))) 
+
+
+#     model.train()
+#     for epoch in range(epochs):
+#         data = data.to(device)
+#         target = target.to(device)
+#         optimizer.zero_grad()
+#         output = model(data)
+#         loss = criterion(output, target)
+#         loss.backward()
+#         optimizer.step()
+
+if __name__ == "__main__":
+     pass
